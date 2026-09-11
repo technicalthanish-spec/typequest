8b66d01ae7baf4185a6f59c50358734732185e88
technicalthanish-spec <technicalthanish@gmail.com>
2026-09-11 22:57:57 +0530

    Improve practice, navigation and account recovery

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.jsx";
 import "./styles.css";
+import "./upgrade.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
